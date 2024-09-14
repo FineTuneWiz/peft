491afd1f6470d6d7e7da06e9c99d0368d88f962e
FineTuneWiz <finetunewiz@gmail.com>
2024-09-15 04:03:23 +0530

    Minor fixes

--- src/peft/tuners/lora/layer.py
+++ src/peft/tuners/lora/layer.py
@@ -603,7 +603,7 @@ class Linear(nn.Module, LoraLayer):
 
                 if not self.use_dora[active_adapter]:
                     delta_W = lora_B.weight @ lora_A.weight
-                    delta_W = delta_W * self.lora_mask
+                    delta_W = delta_W * self.mask
                     # if active_adapter in self.lora_mask:
                     #     mask = self.lora_mask[active_adapter]
                     #     delta_W = delta_W * mask
